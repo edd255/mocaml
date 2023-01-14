a3f9660bfc2faed8e7568a85f0b3410396b8eb6e
abaddon <abaddon@disroot.org>
2023-01-14 17:25:13 +0100

    feat: print the results

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,13 @@
 open Mocaml
 
 let _ = 
-  match read_line() with
-    | line -> Mocaml.eval line
+  Printf.printf "Mini-OCaml\n";
+  while (true) do
+    Printf.printf ">>> ";
+    let _ = match read_line() with
+      | line -> match Mocaml.eval line with
+        | Bval b -> Printf.printf "%b\n" b
+        | Ival i -> Printf.printf "%d\n" i
+        | _      -> Printf.printf "\n"
+    in ()
+  done
